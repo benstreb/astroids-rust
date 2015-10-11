ba0c1386340a212798dc805213ae6e417a4078e0
Benjamin Streb <Bstreb@gmail.com>
2015-10-11 11:36:32 -0700

    Added a spaceship struct
    Instead of having some random local variables

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,23 @@ use opengl_graphics::{GlGraphics, OpenGL};
 use graphics::polygon::Polygon;
 use graphics::{DrawState, Transformed};
 
+struct Spaceship {
+    x: f64,
+    y: f64,
+}
+
+const SPACESHIP_POINTS: [[f64; 2]; 3] = [
+    [50.0, 0.0],
+    [0.0, 50.0],
+    [50.0, 50.0],
+];
+
+impl Spaceship {
+    fn new() -> Spaceship {
+        return Spaceship{x: 0.0, y: 0.0};
+    }
+}
+
 fn main() {
     let opengl = OpenGL::V3_2;
 
@@ -26,14 +43,7 @@ fn main() {
     let ds = DrawState::new();
 
     let poly = Polygon::new([0.0, 0.0, 0.0, 1.0]);
-    let points = [
-        [50.0, 0.0],
-        [0.0, 50.0],
-        [50.0, 50.0],
-    ];
-    let mut x = 0.0;
-    let mut y = 0.0;
-
+    let mut spaceship = Spaceship::new();
 
     for e in window.events() {
         use graphics::clear;
@@ -42,14 +52,14 @@ fn main() {
         match e {
             Event::Render(r) => gl.draw(r.viewport(), |c, gl| {
                 clear(WHITE, gl);
-                poly.draw(&points, &ds, c.transform.trans(x, y), gl)
+                poly.draw(&SPACESHIP_POINTS, &ds, c.transform.trans(spaceship.x, spaceship.y), gl)
             }),
             Event::Input(Input::Press(Button::Keyboard(k))) => 
                 match k {
-                    Key::Up => y -= 1.0,
-                    Key::Down => y += 1.0,
-                    Key::Left => x -= 1.0,
-                    Key::Right => x += 1.0,
+                    Key::Up => spaceship.y -= 1.0,
+                    Key::Down => spaceship.y += 1.0,
+                    Key::Left => spaceship.x -= 1.0,
+                    Key::Right => spaceship.x += 1.0,
                     Key::Q => return,
                     _ => (),
                 },
