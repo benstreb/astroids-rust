2ab14dcf8ca22d18dbd4951c17401fadb8d07f95
Benjamin Streb <Bstreb@gmail.com>
2015-10-11 22:22:58 -0700

    Added rotation and velocity
    Rather than simply moving the ship at a fixed speed based on the most
    recently pressed key
    Also includes a minor visual fix to help with the rotation animation
    The handling still doesn't quite match the original game, so some more
    work needs to be done there

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,31 @@ use graphics::{DrawState, Transformed};
 struct Spaceship {
     x: f64,
     y: f64,
+    dx: f64,
+    dy: f64,
+    theta: f64,
 }
 
 const SPACESHIP_POINTS: [[f64; 2]; 3] = [
-    [5.0, 10.0],
-    [-5.0, 10.0],
-    [0.0, -10.0],
+    [5.0, 7.0],
+    [-5.0, 7.0],
+    [0.0, -13.0],
 ];
 
 impl Spaceship {
     fn new() -> Spaceship {
-        return Spaceship{x: 100.0, y: 100.0};
+        return Spaceship{
+            x: 100.0,
+            y: 100.0,
+            dx: 0.0,
+            dy: 0.0,
+            theta: 0.0,
+        };
+    }
+
+    fn accelerate(&mut self, ddv: f64) {
+        self.dx += self.theta.sin()*ddv;
+        self.dy -= self.theta.cos()*ddv;
     }
 }
 
@@ -52,16 +66,28 @@ fn main() {
         use graphics::clear;
 
         match e {
+            Event::Update(u) => {
+                spaceship.x = (spaceship.x + spaceship.dx*u.dt + 200.0) % 200.0;
+                spaceship.y = (spaceship.y + spaceship.dy*u.dt + 200.0) % 200.0;
+            },
             Event::Render(r) => gl.draw(r.viewport(), |c, gl| {
                 clear(BLACK, gl);
-                poly.draw(&SPACESHIP_POINTS, &ds, c.transform.trans(spaceship.x, spaceship.y), gl)
+                poly.draw(
+                    &SPACESHIP_POINTS,
+                    &ds,
+                    c.transform
+                        .trans(spaceship.x, spaceship.y)
+                        .rot_rad(spaceship.theta),
+                    gl,
+                )
             }),
-            Event::Input(Input::Press(Button::Keyboard(k))) => 
+            Event::Input(Input::Press(Button::Keyboard(k))) =>
                 match k {
-                    Key::Up => spaceship.y -= 1.0,
-                    Key::Down => spaceship.y += 1.0,
-                    Key::Left => spaceship.x -= 1.0,
-                    Key::Right => spaceship.x += 1.0,
+                    Key::Up => spaceship.accelerate(2.0),
+                    Key::Down => spaceship.accelerate(-2.0),
+                    Key::Left => spaceship.theta -= 0.1,
+                    Key::Right => spaceship.theta += 0.1,
+                    Key::R => spaceship = Spaceship::new(),
                     Key::Q => return,
                     _ => (),
                 },
