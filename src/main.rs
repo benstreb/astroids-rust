cb406733d2569477c509424d6313134e3ceaa468
Benjamin Streb <Bstreb@gmail.com>
2015-10-13 23:26:47 -0700

    Added the firing of bullets

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use graphics::polygon::Polygon;
 use graphics::{DrawState, Transformed};
+use graphics::rectangle;
 
 struct Spaceship {
     x: f64,
@@ -21,6 +22,8 @@ struct Spaceship {
     reverse: f64,
     left: f64,
     right: f64,
+    firing: bool,
+    cooldown: f64,
 }
 
 const SPACESHIP_POINTS: [[f64; 2]; 3] = [
@@ -41,6 +44,8 @@ impl Spaceship {
             reverse: 0.0,
             left: 0.0,
             right: 0.0,
+            firing: false,
+            cooldown: 0.0,
         };
     }
 
@@ -52,6 +57,31 @@ impl Spaceship {
 
     fn turn(&mut self) {
         self.theta += self.right - self.left;
+    }
+}
+
+struct Bullet {
+    x: f64,
+    y: f64,
+    theta: f64,
+    distance: f64,
+}
+
+impl Bullet {
+    fn new(x: f64, y: f64, theta: f64) -> Bullet {
+        return Bullet{
+            x: x,
+            y: y,
+            theta: theta,
+            distance: 0.0,
+        };
+    }
+
+    fn go(&mut self, dt: f64, x_max: f64, y_max: f64) {
+        let v = 5.0;
+        self.x = (self.x + self.theta.sin()*v*dt + x_max) % x_max;
+        self.y = (self.y - self.theta.cos()*v*dt + y_max) % y_max;
+        self.distance += v*dt;
     }
 }
 
@@ -74,6 +104,7 @@ fn main() {
 
     let poly = Polygon::new(WHITE);
     let mut spaceship = Spaceship::new();
+    let mut bullets = Vec::new();
 
     for e in window.events() {
         use graphics::clear;
@@ -84,6 +115,15 @@ fn main() {
                 spaceship.turn();
                 spaceship.x = (spaceship.x + spaceship.dx*u.dt + 200.0) % 200.0;
                 spaceship.y = (spaceship.y + spaceship.dy*u.dt + 200.0) % 200.0;
+                spaceship.cooldown = (spaceship.cooldown - u.dt).max(0.0);
+                if spaceship.firing && spaceship.cooldown == 0.0 {
+                    spaceship.cooldown = 1.0;
+                    bullets.push(Bullet::new(spaceship.x, spaceship.y, spaceship.theta));
+                }
+                for ref mut bullet in bullets.iter_mut() {
+                    bullet.go(u.dt, 200.0, 200.0);
+                }
+                bullets.retain(|b| b.distance < 10.0);
             },
             Event::Render(r) => gl.draw(r.viewport(), |c, gl| {
                 clear(BLACK, gl);
@@ -94,7 +134,10 @@ fn main() {
                         .trans(spaceship.x, spaceship.y)
                         .rot_rad(spaceship.theta),
                     gl,
-                )
+                );
+                for bullet in bullets.iter() {
+                    rectangle(WHITE, rectangle::square(bullet.x, bullet.y, 2.0), c.transform, gl);
+                }
             }),
             Event::Input(Input::Press(Button::Keyboard(k))) =>
                 match k {
@@ -102,6 +145,7 @@ fn main() {
                     Key::Down => spaceship.reverse = 1.0,
                     Key::Left => spaceship.left = 0.05,
                     Key::Right => spaceship.right = 0.05,
+                    Key::Space => spaceship.firing = true,
                     Key::R => spaceship = Spaceship::new(),
                     Key::Q => return,
                     _ => (),
@@ -112,6 +156,7 @@ fn main() {
                     Key::Down => spaceship.reverse = 0.0,
                     Key::Left => spaceship.left = 0.0,
                     Key::Right => spaceship.right = 0.0,
+                    Key::Space => spaceship.firing = false,
                     _ => (),
                 },
             _ => (),
